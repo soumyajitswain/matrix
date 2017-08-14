12a3015f67482d199dccc79d91db8ef83372ab39
soumyajitswain <soumyajit.swain@gmail.com>
2017-08-15 00:14:38 +0530

    Spring boot plugin integrated

--- recommendation/src/main/java/com/matrix/Initializer.java
+++ recommendation/src/main/java/com/matrix/Initializer.java
@@ -1,10 +1,17 @@
 package com.matrix;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
 public class Initializer {
 
 	public static void main(String[] args) {
-		
-
+			new SpringApplicationBuilder()
+				.sources(Initializer.class)
+				.run(args);
 	}
 
 }
