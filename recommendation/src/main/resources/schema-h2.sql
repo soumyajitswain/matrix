5ebca32daeb50999be9617d999b38baf06a52449
soumyajitswain <soumyajit.swain@gmail.com>
2017-08-15 10:31:39 +0530

    Default schema is created

---
+++ recommendation/src/main/resources/schema-h2.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS `SKU`;
+CREATE TABLE  `SKU` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `beta` double NOT NULL,
+  `feature1Name` varchar(255) DEFAULT NULL,
+  `feature1Value` double NOT NULL,
+  `feature2Name` varchar(255) DEFAULT NULL,
+  `feature2Value` double NOT NULL,
+  `feature3Name` varchar(255) DEFAULT NULL,
+  `feature3Value` double NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+DROP TABLE IF EXISTS `USER`;
+CREATE TABLE  `USER` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `feature1Name` varchar(255) DEFAULT NULL,
+  `feature1Value` double NOT NULL,
+  `feature2Name` varchar(255) DEFAULT NULL,
+  `feature2Value` double NOT NULL,
+  `feature3Name` varchar(255) DEFAULT NULL,
+  `feature3Value` double NOT NULL,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL,
+  `thetha` double NOT NULL,
+  PRIMARY KEY (`id`)
+);
+DROP TABLE IF EXISTS `USERSKUMATRIX`;
+CREATE TABLE  `USERSKUMATRIX` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `rank` double NOT NULL,
+  `skuId` bigint(20) DEFAULT NULL,
+  `userId` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`userId`) REFERENCES `USER` (`id`),
+  FOREIGN KEY (`skuId`) REFERENCES `SKU` (`id`)
+);
